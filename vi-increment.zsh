3217744f580112ce306b5f733ee9d1a482bdc318
xPMo <PMo_@vivaldi.net>
2019-03-10 07:31:01 -0500

    wrapper functions

--- vi-increment.zsh
+++ vi-increment.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
+vi-increment(){ _vi-increment a }
+vi-decrement(){ _vi-increment x }
+
 fpath+="${0:h}"
-autoload -Uz vi-increment
+autoload -Uz _vi-increment
 zle -N vi-increment
+zle -N vi-decrement
 for m in vicmd viopp; do
 	bindkey -M "$m" '^a' vi-increment
-	bindkey -M "$m" '^x' vi-increment
+	bindkey -M "$m" '^x' vi-decrement
 done
