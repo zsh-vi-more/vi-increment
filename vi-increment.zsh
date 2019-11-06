afe3674b0a76ae25ff84888f1c1c41c4ff72ed81
xPMo <PMo_@vivaldi.net>
2019-11-06 00:07:56 -0600

    Better fpath handling

--- vi-increment.zsh
+++ vi-increment.zsh
@@ -1,9 +1,17 @@
 #!/usr/bin/env zsh
+# {{{ Handle fpath/$0
+# ref: zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+if [[ $zsh_loaded_plugins[-1] != */vi-increment && -z $fpath[(r)${0:h}] ]]
+then
+    fpath+=( "${0:h}" )
+fi
+# }}}
 
 .vi-increment(){ .vi-increment_ a }
 .vi-decrement(){ .vi-increment_ x }
 
-fpath+="${0:h}"
 autoload -Uz .vi-increment_
 zle -N vi-increment
 zle -N vi-decrement
